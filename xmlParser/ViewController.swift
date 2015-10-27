ee585d0987b7874383ab7f2eb17ccb2185bf88e6
Yosemite <yosemite@Yosemites-Mac-mini.local>
2015-10-27 15:34:25 +0530

    XML Url parser - get a xml  value to create table view display the data

--- xmlParser/ViewController.swift
+++ xmlParser/ViewController.swift
@@ -8,18 +8,95 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSXMLParserDelegate {
+    
+    var array = NSMutableArray()
+    var elements = NSMutableDictionary()
+    var element = NSString()
+    var title1 = NSMutableString()
+    var description1 = NSMutableString()
+    var parser = NSXMLParser()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var url = NSURL(string: "http://images.apple.com/au/main/rss/hotnews/hotnews.rss")
+        var xmlParser = NSXMLParser(contentsOfURL: url!)
+        xmlParser?.delegate = self
+        xmlParser?.parse()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func parserDidStartDocument(parser: NSXMLParser)
+    {
+        println("parserDidStartDocument")
+    }
+    
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [NSObject : AnyObject])
+    {
+        element = elementName
+        if(elementName as NSString).isEqualToString("item")
+        {
+                elements = NSMutableDictionary()
+                elements = [:]
+                title1 = NSMutableString()
+                title1 = ""
+                description1 = NSMutableString()
+                description1 = ""
 
+        }
+    }
+    
+    func parser(parser: NSXMLParser, foundCharacters string: String?)
+    {
+        if element.isEqualToString("title")
+        {
+            title1.appendString(string!)
+        } else if element.isEqualToString("description")
+        {
+            description1.appendString(string!)
+        }
+    }
+   
+    
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?)
+    {
+        if (elementName as NSString).isEqualToString("item")
+        {
+                if !title1.isEqual(nil)
+                {
+                    elements.setObject(title1, forKey: "title")
+                }
+                if !description1.isEqual(nil) {
+                    elements.setObject(description1, forKey: "description")
+                }
+                array.addObject(elements)
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    
+    {
+        return array.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+
+    {
+        var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+                
+        cell.textLabel?.text = array.objectAtIndex(indexPath.row).valueForKey("title") as! NSString as String
+        cell.detailTextLabel?.text = array.objectAtIndex(indexPath.row).valueForKey("description") as! NSString as String
+        
+        return cell
+    }
 
 }
 
